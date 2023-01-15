4c3a92b062aaacc7e6423126324e9dcc4ce9cd61
Pierre BEJIAN <pbejian@free.fr>
2023-01-15 21:46:12 +0100

    tralala

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import find_packages
 from setuptools import setup
 
-with open('requirements.txt') as f:
-    content = f.readlines()
-requirements = [x.strip() for x in content if 'git+' not in x]
+# with open('requirements.txt') as f:
+#    content = f.readlines()
+# requirements = [x.strip() for x in content if 'git+' not in x]
 
 setup(name='cubestools',
       version="0.2",
@@ -12,7 +12,7 @@ setup(name='cubestools',
       author_email="pierre.bejian@gmail.com",
       url="https://github.com/pbejian/cubestools",
       packages=find_packages(),
-      install_requires=requirements,
+      # install_requires=requirements,
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
